1d78d24002788d1d6e0a8ffe793462917e2afb08
sid05 <sid05@DESKTOP-FRQ3J32>
2019-09-08 18:52:32 +0900

    주석추가

--- kakao_pay_exam/src/main/java/com/exam/springhome/ctl/BranchCtl.java
+++ kakao_pay_exam/src/main/java/com/exam/springhome/ctl/BranchCtl.java
@@ -30,22 +30,43 @@ public class BranchCtl {
 	@Autowired
 	ProcSvc svcProc;
 	
+	/**
+	 * 입력년도의 관리점의 거래금액 합계를 내림차순으로 정렬하여 반환  하기위한 Controller
+	 * @param year
+	 * @return 
+	 */
 	@GetMapping("/order-by-samt/{year}")
 	public List<Map<String, Object>> getOrderBySamt(@PathVariable String year) { 
 		return svcProc.getAmountByYearByBranch(year); 
 	} 
+	/**
+	 * 모든 년도의 관리점의 거래금액 합계를 내림차순으로 정렬하여 반환  하기위한 Controller
+	 * @return 
+	 */
 	@GetMapping("/order-by-samt")
 	public List<Map<String, Object>> getOrderBySamt() { 
 		return svcProc.getAmountByYearByBranch(null); 
 	} 
 	
+	/**
+	 * 입력한 관리점명에 해당하는 거래금액의 합계를 반환 하기위한 Controller
+	 * - 없는 관리점이 입력되거나 관리점이 입력되지 않으면 Exception 처리
+	 * @param map
+	 * @return
+	 * @throws BrNameNotFoundException
+	 */
 	@PostMapping("/samt-by-branch") 
 	public Map<String, Object> samtByBranch(@RequestBody Map<String, Object> map) throws BrNameNotFoundException { 
+		
+		// 입력값이 없을경우 Exception 처리
 		if(map.get("brName") == null || map.get("brName").toString().isEmpty()) {
 			throw new BrNameNotFoundException("br code not found error");
 		}
 		
+		// 서비스 호출
 		Map<String, Object> rtnMap = svcProc.getSumOfBranchAmt(map.get("brName").toString());
+		
+		// 존재하지 않는 관리점일 경우 Exception 처리
 		if(rtnMap.size() == 0) {
 			throw new BrNameNotFoundException("br code not found error");
 		}
@@ -53,6 +74,12 @@ public class BranchCtl {
 		
 	} 
 	
+	/**
+	 * BrNameNotFoundException 이 발생하였을 경우 처리 할 ResponseStatus 및 
+	 * 메세지 처리를 위한 Contoller
+	 * @param e
+	 * @return
+	 */
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(BrNameNotFoundException.class)
 	public Map<String, Object> samtByBranchError(BrNameNotFoundException e) {
--- kakao_pay_exam/src/main/java/com/exam/springhome/ctl/CustCtl.java
+++ kakao_pay_exam/src/main/java/com/exam/springhome/ctl/CustCtl.java
@@ -23,19 +23,37 @@ public class CustCtl {
 	@Autowired
 	ProcSvc svcProc;
 	
+	/**
+	 * 입력년도중 거래금액의 합계가 가장 큰 고객을 출력 하기위한 Controller
+	 * @param year
+	 * @return
+	 */
 	@GetMapping("/get-max-amt/{year}")
 	public List<Map<String, Object>> getMaxAmt(@PathVariable String year) { 
 		return svcProc.getMaxSumAmoutByYear(year); 
 	} 
+	/**
+	 * 전체 거래년도 중 거래금액의 합계가 가장 큰 고객을 출력 하기위한 Controller
+	 * @return
+	 */
 	@GetMapping("/get-max-amt")
 	public List<Map<String, Object>> getMaxAmt() { 
 		return svcProc.getMaxSumAmoutByYear(null); 
 	} 
 	
+	/**
+	 * 입력년도에서 거래가 없었던 고객을 추출 하기위한 Controller
+	 * @param year
+	 * @return
+	 */
 	@GetMapping("/get-no-hist-cust/{year}") 
 	public List<Map<String, Object>> testPage(@PathVariable String year) { 
 		return svcProc.getAccountNoHistory(year);
 	} 
+	/**
+	 * 모든 거래년도에서 거래가 없었던 고객을 추출 하기위한 Controller
+	 * @return
+	 */
 	@GetMapping("/get-no-hist-cust") 
 	public List<Map<String, Object>> testPage() { 
 		return svcProc.getAccountNoHistory(null);
